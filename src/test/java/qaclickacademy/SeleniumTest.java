791e1b07a84f618280edcfd6d2ac7c9d0ed24a0c
shivani <chandel.shivani.17@gmail.com>
2020-10-01 00:59:31 +0530

    this is my first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,24 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void browserAutomation()
+	{
+		System.out.println("browserAutomation");
+	}
+
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI");
+	}
+	
+	@Test()
+	public void elementsUINew()
+	{
+		System.out.println("elementsUINew");
+	}
+}
